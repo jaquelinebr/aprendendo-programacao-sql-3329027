d9d8eaea98d9bfb4d6a9ab8b51ca38493b05e495
Jaqueline Rios <jaquelinebr@gmail.com>
2024-07-12 23:58:37 +0000

    criado coluna id_salario na tabela employes

--- exercicios/1-CRUD/2-alterar-tabelas.sql
+++ exercicios/1-CRUD/2-alterar-tabelas.sql
@@ -1,6 +1,4 @@
--- Na tabela salario_classe altere o atributo "nivel" para que seu tipo de dado seja varchar, com tamanho máximo de 50 caracteres
-
--- Na tabela employees, adicione uma coluna para receber o id_salario
+-- Na tabela salario_classe altere o atributo "nivel" para que seu tipo de dado seja varchar, com tamanho máximo de 50 caracteres:
 
 ALTER TABLE salario_classe
 DROP salario;
@@ -8,3 +6,8 @@ DROP salario;
 
 ALTER TABLE salario_classe
 ADD salario varchar(50); -- recria a tabela salario com um limite de 50 caracteres)
+
+-- Na tabela employees, adicione uma coluna para receber o id_salario:
+
+ALTER TABLE employees
+ADD id_salario INTEGER; --  id_salario seria uma chave estrangeira pse já existisse essa coluna na tabela salario_classe onde temos a chave primaria id
