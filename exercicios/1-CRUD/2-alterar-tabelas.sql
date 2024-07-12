93aaf4480a030ab054fe8f196c32ae933048532a
Jaqueline Rios <jaquelinebr@gmail.com>
2024-07-12 23:17:37 +0000

    alterado tabela salario_classe para 50 caracteres

--- exercicios/1-CRUD/2-alterar-tabelas.sql
+++ exercicios/1-CRUD/2-alterar-tabelas.sql
@@ -1,3 +1,10 @@
 -- Na tabela salario_classe altere o atributo "nivel" para que seu tipo de dado seja varchar, com tamanho máximo de 50 caracteres
 
 -- Na tabela employees, adicione uma coluna para receber o id_salario
+
+ALTER TABLE salario_classe
+DROP salario;
+-- run selected query (apaga a tabela salario)
+
+ALTER TABLE salario_classe
+ADD salario varchar(50); -- recria a tabela salario com um limite de 50 caracteres)
